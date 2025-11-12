970438b9ca9476bc80951d7f856f000679d7fe56
Nils Hölscher <nilhoel1@users.noreply.github.com>
2025-11-12 09:38:25 +0100

    Add devcontainer and scripts. removed pyQT from rewuirements, to be compatible to devcontainer.

---
+++ run_all_variations.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if the number of parallel processes is provided
+if [ -z "$1" ]; then
+  # If not, use the number of available cores
+  if [[ "$(uname)" == "Darwin" ]]; then
+    NUM_PARALLEL=$(sysctl -n hw.ncpu)
+  else
+    NUM_PARALLEL=$(nproc)
+  fi
+  echo "Number of parallel processes not specified. Using $NUM_PARALLEL cores."
+else
+  NUM_PARALLEL=$1
+fi
+
+# Check if the python executable is provided
+if [ -z "$2" ]; then
+  PYTHON_EXEC="python3"
+  echo "Python executable not specified. Using $PYTHON_EXEC."
+else
+  PYTHON_EXEC=$2
+fi
+
+# Define the arrays of parameters to test
+WCETMul=(0.5 1 1.5 5 10)
+Delta_upMul=(10 25 50 100 200)
+Delta_DownMul=(5 10 25 50 100)
+NrTasks=(2 5 25 50 100)
+
+# Create a command for each combination of parameters
+for wcet in "${WCETMul[@]}"; do
+  for up in "${Delta_upMul[@]}"; do
+    for down in "${Delta_DownMul[@]}"; do
+      for tasks in "${NrTasks[@]}"; do
+        echo "echo 'Running with WCETMul=$wcet, Delta_upMul=$up, Delta_DownMul=$down, NrTasks=$tasks'; $PYTHON_EXEC run_careTaking.py --plot_sets --NumberOfTasksPerSet '$tasks' --ct_wcet_mult '$wcet' --ct_d_up_mult '$up' --ct_d_down_mult '$down'"
+      done
+    done
+  done
+done | xargs -P "$NUM_PARALLEL" -I {} bash -c "{}"
+
+echo "All variations have been processed."
